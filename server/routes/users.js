0f0632fd7d2fe68d20415cd1ad4647a34c80516c
elissa-alarmani <91026581+elissa-alarmani@users.noreply.github.com>
2022-12-26 16:23:38 -0500

    server

---
+++ server/routes/users.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { getUser } from "../controllers/users.js";
+import { verifyToken } from "../middleware/auth.js";
+
+const router = express.Router();
+
+router.get("/:id", verifyToken, getUser);
+
+export default router;
